6c0b0482a0ae913db5240c9177e1f1c87635a10d
Érico Andrei <ericof@gmail.com>
2022-10-10 11:56:02 +0200

    Comment Upgrade tests during

--- {{ cookiecutter.project_slug }}/backend/src/{{ cookiecutter.python_package_name }}/src/{{ cookiecutter.python_package_name }}/tests/test_upgrades.py
+++ {{ cookiecutter.project_slug }}/backend/src/{{ cookiecutter.python_package_name }}/src/{{ cookiecutter.python_package_name }}/tests/test_upgrades.py
@@ -1,46 +1,40 @@
 """Upgrades tests for this package."""
-from parameterized import parameterized
-from plone.app.testing import setRoles
-from plone.app.testing import TEST_USER_ID
-from {{ cookiecutter.python_package_name }}.testing import {{ cookiecutter.__python_package_name_upper }}_INTEGRATION_TESTING  # noqa: E501
-from Products.GenericSetup.upgrade import listUpgradeSteps
+# from parameterized import parameterized
+# from plone.app.testing import setRoles
+# from plone.app.testing import TEST_USER_ID
+# from {{ cookiecutter.python_package_name }}.testing import {{ cookiecutter.__python_package_name_upper }}_INTEGRATION_TESTING  # noqa: E501
+# from Products.GenericSetup.upgrade import listUpgradeSteps
 
-import unittest
+# import unittest
 
 
-class UpgradeStepIntegrationTest(unittest.TestCase):
+# class UpgradeStepIntegrationTest(unittest.TestCase):
 
-    layer = {{ cookiecutter.__python_package_name_upper }}_INTEGRATION_TESTING
-    profile = "{{ cookiecutter.python_package_name }}:default"
+#     layer = {{ cookiecutter.__python_package_name_upper }}_INTEGRATION_TESTING
+#     profile = "{{ cookiecutter.python_package_name }}:default"
 
-    def setUp(self):
-        self.portal = self.layer["portal"]
-        self.setup = self.portal["portal_setup"]
-        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+#     def setUp(self):
+#         self.portal = self.layer["portal"]
+#         self.setup = self.portal["portal_setup"]
+#         setRoles(self.portal, TEST_USER_ID, ["Manager"])
 
-    def _match(self, item, source, dest):
-        source, dest = tuple([source]), tuple([dest])
-        return item["source"] == source and item["dest"] == dest
+#     def _match(self, item, source, dest):
+#         source, dest = tuple([source]), tuple([dest])
+#         return item["source"] == source and item["dest"] == dest
 
-    def available_steps(self) -> list:
-        """Test available steps."""
-        steps = listUpgradeSteps(self.setup, self.profile, self.src)
-        steps = [s for s in steps if self._match(s[0], self.src, self.dst)]
-        return steps
+#     def available_steps(self) -> list:
+#         """Test available steps."""
+#         steps = listUpgradeSteps(self.setup, self.profile, self.src)
+#         steps = [s for s in steps if self._match(s[0], self.src, self.dst)]
+#         return steps
 
-    def test_available(self):
-        """Test upgrade step is available."""
-        if self.src and self.dst:
-            steps = self.available_steps()
-            self.assertEqual(len(steps), 1)
-
-    # # Example of upgrade step test
-    # @parameterized.expand(
-    #     [
-    #         ("{{ cookiecutter.__profile_version }}", "20230229001", 1),
-    #     ]
-    # )
-    # def test_available(self, src, dst, expected):
-    #     """Test upgrade step is available."""
-    #     steps = self.available_steps(src, dst)
-    #     self.assertEqual(len(steps), expected)
+#     # Example of upgrade step test
+#     @parameterized.expand(
+#         [
+#             ("{{ cookiecutter.__profile_version }}", "20230229001", 1),
+#         ]
+#     )
+#     def test_available(self, src, dst, expected):
+#         """Test upgrade step is available."""
+#         steps = self.available_steps(src, dst)
+#         self.assertEqual(len(steps), expected)
